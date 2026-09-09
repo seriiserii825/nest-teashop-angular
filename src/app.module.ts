416d165a6096fa62fb663009266428f4ba6fa588
serii burduja <seriiburduja@gmail.com>
2026-09-09 13:18:00 +0300

    feat: added port to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,15 +10,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-
         host: config.get('DB_HOST'),
         port: config.get<number>('DB_PORT'),
         username: config.get('DB_USERNAME'),
         password: config.get('DB_PASSWORD'),
         database: config.get('DB_DATABASE'),
-
         autoLoadEntities: true,
-
         synchronize: false,
       }),
     }),
