3a28717ea587872e2fb43f9a7e15abf1a81334c3
serii burduja <seriiburduja@gmail.com>
2026-09-14 09:35:50 +0300

    feat: setup cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
+import cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+    credential: true,
+    expandHeaders: 'set-cookie',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
