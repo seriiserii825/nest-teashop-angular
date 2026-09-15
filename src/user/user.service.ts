5f2e1ab2cc3e76dd52b93987a7e32e3b5ef7cd7f
seriiserii825 <seriiburduja@gmail.com>
2026-09-15 22:34:26 +0300

    feat: user service toggle favorite

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { hash } from 'argon2';
 import { Repository } from 'typeorm';
 import { AuthDto } from '../auth/dto/auth.dto.js';
+import { Product } from '../product/entities/product.entity.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { User } from './entities/user.entity.js';
 
@@ -52,5 +53,41 @@ export class UserService {
 
   remove(id: string): string {
     return `This action removes a #${id} user`;
+  }
+
+  async toggleFavoriteProduct(
+    userId: string,
+    productId: string,
+  ): Promise<User> {
+    return this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.favorites', 'product')
+      .where('user.id = :userId', { userId })
+      .getOne()
+      .then(async (user) => {
+        if (!user) {
+          throw new NotFoundException(`User with id ${userId} not found`);
+        }
+        const isFavorite = user.favorites.some(
+          (product) => product.id === productId,
+        );
+        if (isFavorite) {
+          user.favorites = user.favorites.filter(
+            (product) => product.id !== productId,
+          );
+        } else {
+          const product = await this.userRepository.manager.findOne(Product, {
+            where: { id: productId },
+          });
+          if (!product) {
+            throw new NotFoundException(
+              `Product with id ${productId} not found`,
+            );
+          }
+          user.favorites.push(product);
+        }
+        await this.userRepository.save(user);
+        return this.findOne(userId);
+      });
   }
 }
