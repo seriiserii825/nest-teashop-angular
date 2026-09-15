96b2147ee11f7e27250579f2f7b044b345338cfc
serii burduja <seriiburduja@gmail.com>
2026-09-15 09:28:11 +0300

    feat: auth conroller login and register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
+import { AuthDto } from './dto/auth.dto.js';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
+  }
+
+  @HttpCode(201)
+  @Post('register')
+  async register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
+import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { setupSwagger } from './config/swagger.config.js';
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   setupSwagger(app);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
     credential: true,
