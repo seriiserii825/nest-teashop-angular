10d4a5d058b806717f3c4c4bcdc2f821d0eaad30
serii burduja <seriiburduja@gmail.com>
2026-09-22 13:52:58 +0300

    feat: fixed statistics

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { StatisticService } from './statistic.service.js';
 import { Auth } from '../auth/decorators/auth.decorator.js';
-import { CurrentUser } from '../user/decorators/user.decorator.js';
+import { StatisticService } from './statistic.service.js';
 
 @Controller('statistic')
 export class StatisticController {
@@ -9,19 +8,13 @@ export class StatisticController {
 
   @Auth()
   @Get('main/store/:storeId')
-  async getMainStatistic(
-    @CurrentUser('id') userId: string,
-    @Param('storeId') storeId: string,
-  ) {
-    return this.statisticService.getMainStatistic(userId, storeId);
+  async getMainStatistic(@Param('storeId') storeId: string) {
+    return this.statisticService.getMainStatistic(storeId);
   }
 
   @Auth()
   @Get('middle/store/:storeId')
-  async getMiddleStatistics(
-    @CurrentUser('id') userId: string,
-    @Param('storeId') storeId: string,
-  ) {
-    return this.statisticService.getMiddleStatistics(userId, storeId);
+  async getMiddleStatistics(@Param('storeId') storeId: string) {
+    return this.statisticService.getMiddleStatistics(storeId);
   }
 }
