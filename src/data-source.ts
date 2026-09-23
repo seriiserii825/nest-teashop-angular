ede5ad88e5ac16dc17092fb113aae56a6b89405c
seriiserii825 <seriiburduja@gmail.com>
2026-09-23 20:01:27 +0300

    bug-fix: package json migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,12 @@
 import { DataSource } from 'typeorm';
 import { databaseConfig } from './database.config.js';
 
+const isCompiled = import.meta.url.endsWith('.js');
+const root = isCompiled ? 'dist' : 'src';
+const ext = isCompiled ? 'js' : 'ts';
+
 export default new DataSource({
   ...databaseConfig,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  entities: [`${root}/**/*.entity.${ext}`],
+  migrations: [`${root}/migrations/*.${ext}`],
 });
